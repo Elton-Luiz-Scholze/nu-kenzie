d586650957a34bf9db61f4b8787be14810018b56
Elton Luiz Scholze <elscholze@gmail.com>
2022-09-26 01:48:00 -0300

    fix: fix App.jsx, List/index.jsx

--- react-entrega-s1-nu-kenzie-elton-luiz-scholze/src/App.jsx
+++ react-entrega-s1-nu-kenzie-elton-luiz-scholze/src/App.jsx
@@ -2,19 +2,26 @@ import { useState } from "react";
 import "./App.css";
 import { Form } from "./components/Form";
 import { List } from "./components/List";
+import NuKenzie from "./assets/NuKenzie.svg";
 
 function App() {
   const [listTransactions, setListTransactions] = useState([]);
 
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <img src={NuKenzie} alt="Logo NuKenzie" />
+        <button type="button">Início</button>
+      </header>
       <main className="container">
         <Form
           listTransactions={listTransactions}
           setListTransactions={setListTransactions}
         />
-        <List listTransactions={listTransactions} />
+        <List
+          listTransactions={listTransactions}
+          setListTransactions={setListTransactions}
+        />
       </main>
     </div>
   );
--- react-entrega-s1-nu-kenzie-elton-luiz-scholze/src/components/List/index.jsx
+++ react-entrega-s1-nu-kenzie-elton-luiz-scholze/src/components/List/index.jsx
@@ -1,20 +1,34 @@
 import imgLixeira from "../../assets/trash.svg";
+import noCard from "../../assets/NoCard.svg";
 
-export function List({ listTransactions }) {
+export function List({ listTransactions, setListTransactions }) {
+  function deleteTransaction(index) {
+    const transactionFiltered = listTransactions.filter(
+      (transaction, transactionIndex) => transactionIndex !== index
+    );
+    setListTransactions(transactionFiltered);
+  }
+
   return (
     <ul>
-      {listTransactions.map((transaction, index) => (
-        <li key={index}>
-          <div>
-            <p>{transaction.description}</p>
-            <span>{transaction.type}</span>
-          </div>
-          <div>
-            <p>{transaction.value}</p>
-            <img src={imgLixeira} alt="" />
-          </div>
-        </li>
-      ))}
+      {listTransactions.length ? (
+        listTransactions.map((transaction, index) => (
+          <li key={index}>
+            <div>
+              <p>{transaction.description}</p>
+              <span>{transaction.type}</span>
+            </div>
+            <div>
+              <p>{transaction.value}</p>
+              <button onClick={() => deleteTransaction(index)}>
+                <img src={imgLixeira} alt="" />
+              </button>
+            </div>
+          </li>
+        ))
+      ) : (
+        <img src={noCard} alt="" />
+      )}
     </ul>
   );
 }
